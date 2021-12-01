cb67a40dcc2b8887477e39e8e60dd1b7cd48e972
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-01 14:25:36 +0100

    New Atlanta

--- app/src/main/java/com/example/myapplicationempty/MainActivity.java
+++ app/src/main/java/com/example/myapplicationempty/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplicationempty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG="acitivity one";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_aciticity_one);
         //TextView Phone_text= (TextView) findViewById(R.id.Phone);
         //Phone_text.setText("+212 *");
-        
+        Log.i(TAG,"---onCreate--");
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    
+
+
 }
