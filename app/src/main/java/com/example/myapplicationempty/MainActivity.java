527ba130949b7e1efcb2ff41a03524b1ba856ed1
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-01 14:29:07 +0100

    New Atlanta

--- app/src/main/java/com/example/myapplicationempty/MainActivity.java
+++ app/src/main/java/com/example/myapplicationempty/MainActivity.java
@@ -22,30 +22,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(TAG,"---onStart--");
+
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.i(TAG,"---onRestart--");
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i(TAG,"---onResume--");
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.i(TAG,"---onPause--");
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i(TAG,"---onStop--");
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.i(TAG,"---onDestroy--");
+
     }
 }
