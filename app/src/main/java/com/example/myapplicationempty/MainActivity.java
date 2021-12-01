c765bcc37be26146f1f0cfe51694957d6a784b65
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-01 16:42:28 +0100

    New Atlanta

--- app/src/main/java/com/example/myapplicationempty/MainActivity.java
+++ app/src/main/java/com/example/myapplicationempty/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_view_new);
+        setContentView(R.layout.activity_main);
         //TextView Phone_text= (TextView) findViewById(R.id.Phone);
         //Phone_text.setText("+212 *");
         Log.i(TAG,"---onCreate--");
